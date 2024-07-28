f2075467cc44fca07636afe2aad8fee97f5b8f3b
Tony Jinks <tony.jinks@gmail.com>
2024-07-28 15:11:59 +0100

    Fixed ep captures. Various other changes.

---
+++ TjChess/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.h
+//  TjChess
+//
+//  Created by Tony on 25/07/2024.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#include "
+
+#endif /* BridgingHeader_h */
